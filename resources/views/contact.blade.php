7400e4ff83daab6fc302e843c50756a146a9d3e9
kom4 <damir.komic@gmail.com>
2018-05-25 12:44:33 +0200

    posao 25.05.2018.

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,5 +3,9 @@
 
 @section('content')
 <h1>CONTACT</h1>
-<p class="text-justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos natus dolores vero labore dolorum nostrum sequi odio ea eos. Dignissimos assumenda, obcaecati dolor veritatis illo impedit nihil fuga dolorum eum, quae architecto, nam dolorem? Sequi aperiam ut recusandae suscipit, non quis. Fugiat harum sequi exercitationem possimus dolore ad velit ab accusantium explicabo impedit ratione libero, deserunt animi odio repellendus quo? Nesciunt in beatae obcaecati debitis consectetur praesentium itaque ipsam pariatur placeat cum. Blanditiis necessitatibus ipsum dolores. Libero, a ratione itaque aliquam, consequuntur neque ducimus saepe rerum vero totam debitis dignissimos? Veritatis impedit facilis, quas autem quasi perferendis iusto facere aliquid! Accusamus voluptates quis minima sunt architecto itaque harum impedit velit! Illo tenetur ducimus optio possimus aliquid libero ea aliquam? Laborum placeat cupiditate itaque unde, debitis alias id dignissimos nisi, sequi pariatur, quasi sunt atque nulla! Dolore fugiat ipsum explicabo? Rerum, modi hic illo reiciendis sequi nobis temporibus corrupti fuga inventore?</p>
+
+{!! Form::open(['url' => 'contact/submit']) !!}
+
+{!! Form::close() !!}
+
 @endsection
